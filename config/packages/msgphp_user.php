fb56c8a81eb9f2bf346c018d9c94addf8edfcfc1
Roland Franssen <franssen.roland@gmail.com>
2017-10-29 13:50:42 +0100

    added eav package

--- config/packages/msgphp_user.php
+++ config/packages/msgphp_user.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Entity\User\{PendingUser, User, UserRole, UserSecondaryEmail};
+use App\Entity\User\{PendingUser, User, UserAttributeValue, UserRole, UserSecondaryEmail};
 use App\Security\UserRoleProvider;
 use MsgPhp\User\Infra\Security\UserRoleProviderInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -8,6 +8,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return function (ContainerConfigurator $container) {
     $container->extension('msgphp_user', [
         'pending_user_class' => PendingUser::class,
+        'user_attribute_value_class' => UserAttributeValue::class,
         'user_class' => User::class,
         'user_role_class' => UserRole::class,
         'user_secondary_email_class' => UserSecondaryEmail::class,
--- src/DataFixtures/ORM/Users.php
+++ src/DataFixtures/ORM/Users.php
@@ -2,11 +2,15 @@
 
 namespace App\DataFixtures\ORM;
 
+use App\Entity\Eav\Attribute;
 use App\Entity\User\User;
+use App\Entity\User\UserAttributeValue;
 use App\Entity\User\UserRole;
 use App\Security\UserRoleProvider;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use MsgPhp\Eav\Infra\Uuid\AttributeId;
+use MsgPhp\Eav\Infra\Uuid\AttributeValueId;
 use MsgPhp\User\Infra\Security\SecurityUser;
 use MsgPhp\User\Infra\Uuid\UserId;
 use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
@@ -20,6 +24,15 @@ final class Users extends Fixture
         $user = $this->createUser('user@domain.dev');
         $user->enable();
         $manager->persist($user);
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $boolAttr = new Attribute(new AttributeId()), true));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $boolAttr, false));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $boolAttr, null));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $intAttr = new Attribute(new AttributeId()), 123));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $intAttr, -123));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $floatAttr = new Attribute(new AttributeId()), 123.1223456789));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $floatAttr, -0.123));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $stringAttr = new Attribute(new AttributeId()), 'text'));
+        $manager->persist(new UserAttributeValue(new AttributeValueId(), $user, $dateTimeAttr = new Attribute(new AttributeId()), new \DateTime()));
 
         $user = $this->createUser('user+disabled@domain.dev');
         $manager->persist($user);
