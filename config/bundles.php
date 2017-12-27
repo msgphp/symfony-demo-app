2d4b80767e65bb55b8b65513402d1e1be939691e
Gawain Lynch <gawain.lynch@gmail.com>
2017-12-27 20:41:54 +0100

    Updates & clean ups (#2)
    * Update project Composer configuration
    
    - Minimum stability to "stable"
    - Add MsgPhp components directly
    - Swap phpunit/phpunit for symfony/phpunit-bridge
    
    * Update version locks
    
    * Add files from migration recipe
    
    * Add changes from symfony/phpunit-bridge recipe

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
