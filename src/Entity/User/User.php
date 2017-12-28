5f541354c3c92748457e8102b21d7459e2cf842c
Roland Franssen <franssen.roland@gmail.com>
2017-12-28 17:25:50 +0100

    Added User::getRoles relation

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -2,8 +2,10 @@
 
 namespace App\Entity\User;
 
+use Doctrine\Common\Collections\{ArrayCollection, Collection};
 use Doctrine\ORM\Mapping as ORM;
 use MsgPhp\User\Entity\User as BaseUser;
+use MsgPhp\User\UserIdInterface;
 
 /**
  * @ORM\Entity()
@@ -12,4 +14,47 @@ use MsgPhp\User\Entity\User as BaseUser;
  */
 class User extends BaseUser
 {
+    /**
+     * @var Collection|UserRole[]
+     * @ORM\OneToMany(targetEntity="UserRole", mappedBy="user", cascade={"persist", "remove"}, orphanRemoval=true)
+     */
+    private $roles;
+
+    public function __construct(UserIdInterface $id, string $email, string $password)
+    {
+        parent::__construct($id, $email, $password);
+
+        $this->roles = new ArrayCollection();
+    }
+
+    public function hasRole(string $role): bool
+    {
+        return in_array($role, $this->getRoles(), true);
+    }
+
+    public function addRole(string $role): void
+    {
+        if (!$this->hasRole($role)) {
+            $this->roles->add(new UserRole($this, $role));
+        }
+    }
+
+    public function removeRole(string $role): void
+    {
+        $this->roles->removeElement(
+            $this->roles->filter(function (UserRole $userRole) use ($role) {
+                return $userRole->getRole() === $role;
+            })->first()
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getRoles(): array
+    {
+        return $this->roles->map(function (UserRole $userRole) {
+            return $userRole->getRole();
+        })->toArray();
+    }
 }
