346df12c876ccc1cdb0a6bb02c6f6e495d162aa5
Roland Franssen <franssen.roland@gmail.com>
2017-12-28 21:04:40 +0100

    Added User::getSecondaryEmails

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -20,11 +20,18 @@ class User extends BaseUser
      */
     private $roles;
 
+    /**
+     * @var Collection|UserSecondaryEmail[]
+     * @ORM\OneToMany(targetEntity="UserSecondaryEmail", mappedBy="user", cascade={"persist", "remove"}, orphanRemoval=true)
+     */
+    private $secondaryEmails;
+
     public function __construct(UserIdInterface $id, string $email, string $password)
     {
         parent::__construct($id, $email, $password);
 
         $this->roles = new ArrayCollection();
+        $this->secondaryEmails = new ArrayCollection();
     }
 
     public function hasRole(string $role): bool
@@ -55,6 +62,37 @@ class User extends BaseUser
     {
         return $this->roles->map(function (UserRole $userRole) {
             return $userRole->getRole();
+        })->toArray();
+    }
+
+    public function hasSecondaryEmail(string $email): bool
+    {
+        return in_array($email, $this->getSecondaryEmails(), true);
+    }
+
+    public function addSecondaryEmail(string $email): void
+    {
+        if (!$this->hasSecondaryEmail($email)) {
+            $this->secondaryEmails->add(new UserSecondaryEmail($this, $email));
+        }
+    }
+
+    public function removeSecondaryEmail(string $email): void
+    {
+        $this->secondaryEmails->removeElement(
+            $this->secondaryEmails->filter(function (UserSecondaryEmail $userSecondaryEmail) use ($email) {
+                return $userSecondaryEmail->getEmail() === $email;
+            })->first()
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getSecondaryEmails(): array
+    {
+        return $this->secondaryEmails->map(function (UserSecondaryEmail $userSecondaryEmail) {
+            return $userSecondaryEmail->getEmail();
         })->toArray();
     }
 }
--- src/Entity/User/UserSecondaryEmail.php
+++ src/Entity/User/UserSecondaryEmail.php
@@ -7,6 +7,9 @@ use MsgPhp\User\Entity\UserSecondaryEmail as BaseUserSecondaryEmail;
 
 /**
  * @ORM\Entity()
+ * @ORM\AssociationOverrides({
+ *     @ORM\AssociationOverride(name="user", inversedBy="secondaryEmails")
+ * })
  *
  * @final
  */
