d7194a546d068bf9df20daa387a55c5613b4ed6e
Roland Franssen <franssen.roland@gmail.com>
2017-12-28 15:53:11 +0100

    remove default doctrine config

--- config/packages/msgphp.php
+++ config/packages/msgphp.php
@@ -8,46 +8,19 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return function (ContainerConfigurator $container) {
     $container->extension('msgphp_eav', [
-        'class_mapping' => $classMappingEav = [
+        'class_mapping' => [
             Attribute::class => \App\Entity\Eav\Attribute::class,
             AttributeValue::class => \App\Entity\Eav\AttributeValue::class,
         ],
     ]);
 
     $container->extension('msgphp_user', [
-        'class_mapping' => $classMappingUser = [
+        'class_mapping' => [
             PendingUser::class => \App\Entity\User\PendingUser::class,
             User::class => \App\Entity\User\User::class,
             UserAttributeValue::class => \App\Entity\User\UserAttributeValue::class,
             UserRole::class => \App\Entity\User\UserRole::class,
             UserSecondaryEmail::class => \App\Entity\User\UserSecondaryEmail::class,
-        ],
-    ]);
-
-    $container->extension('doctrine', [
-        'dbal' => [
-            'types' => [
-                AttributeIdType::NAME => AttributeIdType::class,
-                AttributeValueIdType::NAME => AttributeValueIdType::class,
-                UserIdType::NAME => UserIdType::class,
-            ],
-        ],
-        'orm' => [
-            'resolve_target_entities' => $classMappingEav + $classMappingUser,
-            'mappings' => [
-                'msgphp_attribute' => [
-                    'dir' => '%kernel.project_dir%/vendor/msgphp/eav/Infra/Doctrine/Resources/mapping',
-                    'type' => 'xml',
-                    'prefix' => 'MsgPhp\Eav\Entity',
-                    'is_bundle' => false,
-                ],
-                'msgphp_user' => [
-                    'dir' => '%kernel.project_dir%/vendor/msgphp/user/Infra/Doctrine/Resources/mapping',
-                    'type' => 'xml',
-                    'prefix' => 'MsgPhp\User\Entity',
-                    'is_bundle' => false,
-                ],
-            ],
         ],
     ]);
 };
