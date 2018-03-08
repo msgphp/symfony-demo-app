66becda49cf43b470d86fa1fcc9c2fe2761479dd
Roland Franssen <franssen.roland@gmail.com>
2018-03-08 21:19:22 +0100

    update sensio param converters

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,15 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\User\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
 final class DefaultController
 {
+    /**
+     * @ParamConverter("user", converter="msgphp.current_user")
+     */
     public function __invoke(?User $user, Environment $twig): Response
     {
         return new Response($twig->render('default.html.twig', [
-            'user' => $user,
+            'current_user' => $user,
         ]));
     }
 }
