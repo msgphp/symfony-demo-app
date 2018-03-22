6194bcce081d9a47f28c358a05eef7e66662cde2
Roland Franssen <franssen.roland@gmail.com>
2018-03-22 18:40:37 +0100

    deterministic resource/document IDs

--- src/Api/User/UserDocumentTransformer.php
+++ src/Api/User/UserDocumentTransformer.php
@@ -8,14 +8,20 @@ use PascalDeVink\ShortUuid\ShortUuid;
 
 final class UserDocumentTransformer
 {
+    private const DOC_NS = 'ee5b8c83-f12d-41f5-bcf9-3e83b7558317';
+
     public function __invoke(User $user): array
     {
+        // @todo leverage service for DomainIdInterface types
+        $userId = $user->getId()->toString();
+        $docId = ShortUuid::uuid5(self::DOC_NS, sha1($userId));
+
         return [
             'document_type' => UserResource::class,
-            'document_id' => $id = ShortUuid::uuid4(),
-            'id' => $id,
+            'document_id' => $docId,
+            'id' => $docId,
             'email' => $user->getEmail(),
-            'user_id' => $user->getId()->toString(),
+            'user_id' => $userId,
         ];
     }
 }
