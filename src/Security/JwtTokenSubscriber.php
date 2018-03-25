efeb9eacce4c4c0de913e9c65b3a8d2c8c937999
Roland Franssen <franssen.roland@gmail.com>
2018-03-25 19:28:51 +0200

    revise JWT + API

--- src/Security/JwtTokenSubscriber.php
+++ src/Security/JwtTokenSubscriber.php
@@ -2,14 +2,29 @@
 
 namespace App\Security;
 
+use ApiPlatform\Core\Api\UrlGeneratorInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTFailureEventInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Events;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
-final class JwtTokenErrorHandler implements EventSubscriberInterface
+final class JwtTokenSubscriber implements EventSubscriberInterface
 {
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function handleSuccess(AuthenticationSuccessEvent $event): void
+    {
+        $event->getResponse()->headers->set('foo', 'bar');
+    }
+
     public function handleFailure(JWTFailureEventInterface $event): void
     {
         $exception = $event->getException();
@@ -24,6 +39,7 @@ final class JwtTokenErrorHandler implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
+            Events::AUTHENTICATION_SUCCESS => 'handleSuccess',
             Events::JWT_EXPIRED => 'handleFailure',
             Events::JWT_INVALID => 'handleFailure',
             Events::JWT_NOT_FOUND => 'handleFailure',
