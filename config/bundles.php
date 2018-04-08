ef2e656eab53eaf4506b48fdafc31db1139fcc00
Roland Franssen <franssen.roland@gmail.com>
2018-04-08 10:55:20 +0200

    raise deps + enable debug

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
