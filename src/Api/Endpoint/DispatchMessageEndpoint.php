0410f0e67685daef10bec1129279d8cf20b1bd2b
Roland Franssen <franssen.roland@gmail.com>
2018-07-08 12:54:00 +0200

    sync user projection (#47)

--- src/Api/Endpoint/DispatchMessageEndpoint.php
+++ src/Api/Endpoint/DispatchMessageEndpoint.php
@@ -4,18 +4,16 @@ namespace App\Api\Endpoint;
 
 use App\Api\Projection\UserProjection;
 use MsgPhp\Domain\Projection\ProjectionInterface;
-use MsgPhp\Domain\Projection\ProjectionRepositoryInterface;
 use MsgPhp\User\Command\DeleteUserCommand;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 final class DispatchMessageEndpoint
 {
-    public function __invoke(Request $request, ProjectionInterface $data, ProjectionRepositoryInterface $projectionRepository, MessageBusInterface $bus)
+    public function __invoke(Request $request, ProjectionInterface $data, MessageBusInterface $bus)
     {
         if ($data instanceof UserProjection && $request->isMethod(Request::METHOD_DELETE)) {
             $bus->dispatch(new DeleteUserCommand($data->userId));
-            $projectionRepository->delete(get_class($data), $data->id);
         }
 
         return null;
