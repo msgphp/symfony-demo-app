112877c6cb7da146c2c5133ed87c3b0e0d3cb3c8
Roland Franssen <franssen.roland@gmail.com>
2018-07-09 21:14:03 +0200

    tweak routes

--- config/routes/api.php
+++ config/routes/api.php
@@ -3,6 +3,10 @@
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
+    $routes->import('.', 'api_platform')
+        ->prefix('/api')
+    ;
+
     $routes
         ->add('api_login', '/api/login')
             ->methods(['POST'])
