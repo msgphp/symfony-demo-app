56b36492e4867e622a6c71364d93812041a4b0b7
Roland Franssen <franssen.roland@gmail.com>
2018-08-25 12:46:32 +0200

    finetune

--- src/Api/Endpoint/DispatchMessageEndpoint.php
+++ src/Api/Endpoint/DispatchMessageEndpoint.php
@@ -2,16 +2,20 @@
 
 namespace App\Api\Endpoint;
 
+use App\Api\Projection\Document\DocumentIdentity;
 use App\Api\Projection\UserProjection;
 use MsgPhp\Domain\Projection\ProjectionInterface;
-use MsgPhp\User\Command\DeleteUserCommand;
+use MsgPhp\User\Command\{CreateUserCommand, DeleteUserCommand};
+use MsgPhp\User\Infra\Uuid\UserId;
 use MsgPhp\User\Password\PasswordHashingInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class DispatchMessageEndpoint
 {
-    public function __invoke(Request $request, ProjectionInterface $data, MessageBusInterface $bus, PasswordHashingInterface $passwordHashing)
+    public function __invoke(Request $request, ProjectionInterface $data, MessageBusInterface $bus, UrlGeneratorInterface $urlGenerator, PasswordHashingInterface $passwordHashing, DocumentIdentity $documentIdentity)
     {
         if ($data instanceof UserProjection) {
             if ($request->isMethod(Request::METHOD_DELETE)) {
@@ -21,9 +25,10 @@ final class DispatchMessageEndpoint
             }
 
             if ($request->isMethod(Request::METHOD_POST)) {
-                $data->id = 'foo';
+                $docId = $documentIdentity->identifyId($userId = UserId::fromValue($data->userId));
+                $locationUrl = $urlGenerator->generate('api_users_get_item', ['id' => $docId], UrlGeneratorInterface::ABSOLUTE_URL);
 
-                return $data;
+                return new JsonResponse(['id' => $docId, 'user_id' => $userId->toString()], JsonResponse::HTTP_CREATED, ['Location' => $locationUrl]);
             }
         }
 
