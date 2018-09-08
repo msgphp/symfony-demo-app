757917f5411b0aeac9e50d7686ab2301cf9e4386
Roland Franssen <franssen.roland@gmail.com>
2018-09-08 14:01:48 +0200

    setup one-time login form

--- src/Controller/User/LoginController.php
+++ src/Controller/User/LoginController.php
@@ -3,21 +3,29 @@
 namespace App\Controller\User;
 
 use App\Form\User\LoginType;
+use App\Form\User\OneTimeLoginType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Twig\Environment;
 
 final class LoginController
 {
     public function __invoke(
+        Request $request,
         Environment $twig,
         FormFactoryInterface $formFactory,
-        AuthenticationUtils $authenticationUtils
+        AuthenticationUtils $authenticationUtils,
+        EntityManagerInterface $em
     ): Response
     {
+        // one-time login
+        $oneTimeLoginForm = $formFactory->createNamed('', OneTimeLoginType::class);
+
+        // regular form login
         $form = $formFactory->createNamed('', LoginType::class, [
             'email' => $authenticationUtils->getLastUsername(),
         ]);
@@ -28,6 +36,7 @@ final class LoginController
 
         return new Response($twig->render('user/login.html.twig', [
             'form' => $form->createView(),
+            'oneTimeLoginForm' => $oneTimeLoginForm->createView(),
         ]));
     }
 }
--- src/Security/OneTimeLoginAuthenticator.php
+++ src/Security/OneTimeLoginAuthenticator.php
@@ -2,8 +2,14 @@
 
 namespace App\Security;
 
+use App\Entity\User\OneTimeLoginToken;
+use Doctrine\ORM\EntityManagerInterface;
+use MsgPhp\User\Infra\Security\SecurityUser;
+use MsgPhp\User\Infra\Security\SecurityUserProvider;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -12,153 +18,81 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 final class OneTimeLoginAuthenticator extends AbstractGuardAuthenticator
 {
-    /**
-     * Returns a response that directs the user to authenticate.
-     *
-     * This is called when an anonymous request accesses a resource that
-     * requires authentication. The job of this method is to return some
-     * response that "helps" the user start into the authentication process.
-     *
-     * Examples:
-     *  A) For a form login, you might redirect to the login page
-     *      return new RedirectResponse('/login');
-     *  B) For an API token authentication system, you return a 401 response
-     *      return new Response('Auth header required', 401);
-     *
-     * @param Request $request The request that resulted in an AuthenticationException
-     * @param AuthenticationException $authException The exception that started the authentication process
-     *
-     * @return Response
-     */
+    private $em;
+    private $userProvider;
+    private $urlGenerator;
+
+    public function __construct(EntityManagerInterface $em, SecurityUserProvider $userProvider, UrlGeneratorInterface $urlGenerator)
+    {
+        $this->em = $em;
+        $this->userProvider = $userProvider;
+        $this->urlGenerator = $urlGenerator;
+    }
+
     public function start(Request $request, AuthenticationException $authException = null): Response
     {
-        // TODO: Implement start() method.
+        return new RedirectResponse($this->urlGenerator->generate('login'));
     }
 
-    /**
-     * Does the authenticator support the given Request?
-     *
-     * If this returns false, the authenticator will be skipped.
-     *
-     * @param Request $request
-     *
-     * @return bool
-     */
     public function supports(Request $request): bool
     {
-        // TODO: Implement supports() method.
+        return $request->isMethod(Request::METHOD_GET) && $request->query->has('token');
     }
 
-    /**
-     * Get the authentication credentials from the request and return them
-     * as any type (e.g. an associate array).
-     *
-     * Whatever value you return here will be passed to getUser() and checkCredentials()
-     *
-     * For example, for a form login, you might:
-     *
-     *      return array(
-     *          'username' => $request->request->get('_username'),
-     *          'password' => $request->request->get('_password'),
-     *      );
-     *
-     * Or for an API token that's on a header, you might use:
-     *
-     *      return array('api_key' => $request->headers->get('X-API-TOKEN'));
-     *
-     * @param Request $request
-     *
-     * @return mixed Any non-null value
-     *
-     * @throws \UnexpectedValueException If null is returned
-     */
     public function getCredentials(Request $request)
     {
-        // TODO: Implement getCredentials() method.
+        return $request->query->get('token');
     }
 
-    /**
-     * Return a UserInterface object based on the credentials.
-     *
-     * The *credentials* are the return value from getCredentials()
-     *
-     * You may throw an AuthenticationException if you wish. If you return
-     * null, then a UsernameNotFoundException is thrown for you.
-     *
-     * @param mixed $credentials
-     * @param UserProviderInterface $userProvider
-     *
-     * @throws AuthenticationException
-     *
-     * @return UserInterface|null
-     */
     public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
     {
-        // TODO: Implement getUser() method.
+        if (null === $oneTimeLoginToken = $this->getOneTimeLoginToken($credentials)) {
+            return null;
+        }
+
+        return $this->userProvider->fromUser($oneTimeLoginToken->getUser());
     }
 
-    /**
-     * Returns true if the credentials are valid.
-     *
-     * If any value other than true is returned, authentication will
-     * fail. You may also throw an AuthenticationException if you wish
-     * to cause authentication to fail.
-     *
-     * The *credentials* are the return value from getCredentials()
-     *
-     * @param mixed $credentials
-     * @param UserInterface $user
-     *
-     * @return bool
-     *
-     * @throws AuthenticationException
-     */
     public function checkCredentials($credentials, UserInterface $user): bool
     {
-        // TODO: Implement checkCredentials() method.
+        if (!$user instanceof SecurityUser) {
+            return false;
+        }
+
+        if (null === $oneTimeLoginToken = $this->getOneTimeLoginToken($credentials)) {
+            return false;
+        }
+
+        return $oneTimeLoginToken->getUserId()->equals($user->getUserId());
     }
 
-    /**
-     * Called when authentication executed, but failed (e.g. wrong username password).
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the login page or a 403 response.
-     *
-     * If you return null, the request will continue, but the user will
-     * not be authenticated. This is probably not what you want to do.
-     *
-     * @param Request $request
-     * @param AuthenticationException $exception
-     *
-     * @return Response|null
-     */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        return new RedirectResponse($this->urlGenerator->generate('login'));
     }
 
-    /**
-     * Called when authentication executed and was successful!
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the last page they visited.
-     *
-     * If you return null, the current request will continue, and the user
-     * will be authenticated. This makes sense, for example, with an API.
-     *
-     * @param Request $request
-     * @param TokenInterface $token
-     * @param string $providerKey The provider (i.e. firewall) key
-     *
-     * @return Response|null
-     */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        $this->removeOneTimeLoginToken($this->getCredentials($request));
+
+        return new RedirectResponse($this->urlGenerator->generate('my_account'));
     }
 
     public function supportsRememberMe(): bool
     {
         return false;
+    }
+
+    private function getOneTimeLoginToken(string $token): ?OneTimeLoginToken
+    {
+        return $this->em->find(OneTimeLoginToken::class, $token);
+    }
+
+    private function removeOneTimeLoginToken(string $token): void
+    {
+        if (null !== $oneTimeLoginToken = $this->em->getReference(OneTimeLoginToken::class, $token)) {
+            $this->em->remove($oneTimeLoginToken);
+            $this->em->flush();
+        }
     }
 }
