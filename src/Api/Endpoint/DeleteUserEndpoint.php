297ba612ab5ea8f74a38e5116529023daafb1fe5
Roland Franssen <franssen.roland@gmail.com>
2019-02-09 14:44:47 +0100

    upgrade to 0.9 (#75)

--- src/Api/Endpoint/DeleteUserEndpoint.php
+++ src/Api/Endpoint/DeleteUserEndpoint.php
@@ -6,6 +6,7 @@ namespace App\Api\Endpoint;
 
 use App\Api\Projection\UserProjection;
 use MsgPhp\User\Command\DeleteUserCommand;
+use MsgPhp\User\Infra\Uuid\UserId;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -13,7 +14,7 @@ final class DeleteUserEndpoint
 {
     public function __invoke(Request $request, UserProjection $data, MessageBusInterface $bus)
     {
-        $bus->dispatch(new DeleteUserCommand($data->userId));
+        $bus->dispatch(new DeleteUserCommand(UserId::fromValue($data->userId)));
 
         return null;
     }
