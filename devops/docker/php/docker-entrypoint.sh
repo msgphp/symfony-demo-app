e18eb6774e8ce44c4a1fa0c90543f7ffda3e285f
Roland Franssen <franssen.roland@gmail.com>
2019-06-28 11:18:51 +0200

    cleanup

--- devops/docker/php/docker-entrypoint.sh
+++ devops/docker/php/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 # originally taken from https://github.com/api-platform/api-platform/blob/master/api/docker/php/docker-entrypoint.sh
 set -e
 
@@ -8,16 +8,13 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
-	if [ "$APP_ENV" != 'prod' ]; then
-		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+	PHP_INI_RECOMMENDED="${PHP_INI_DIR}/php.ini-production"
+	if [ "${APP_ENV}" != 'prod' ]; then
+		PHP_INI_RECOMMENDED="${PHP_INI_DIR}/php.ini-development"
 	fi
-	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
-#
+	ln -sf "${PHP_INI_RECOMMENDED}" "${PHP_INI_DIR}/php.ini"
+
 #	mkdir -p var/cache var/log
-#	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-#	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
-#
 #	if [ "$APP_ENV" != 'prod' ]; then
 #		composer install --prefer-dist --no-progress --no-suggest --no-interaction
 #	fi
