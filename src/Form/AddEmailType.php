1ddfb8af55b68935c9275fe42b8a6495a8fddc64
Roland Franssen <franssen.roland@gmail.com>
2019-08-25 07:45:53 +0200

    cleanup

--- src/Form/AddEmailType.php
+++ src/Form/AddEmailType.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use MsgPhp\User\Infrastructure\Validator\UniqueUsername as UniqueEmail;
+use MsgPhp\User\Infrastructure\Validator\UniqueUsername;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,7 +16,7 @@ final class AddEmailType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('email', EmailType::class, [
-            'constraints' => [new NotBlank(), new Email(), new UniqueEmail()],
+            'constraints' => [new NotBlank(), new Email(), new UniqueUsername()],
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Form;
 
 use MsgPhp\User\Infrastructure\Form\Type\HashedPasswordType;
-use MsgPhp\User\Infrastructure\Validator\UniqueUsername as UniqueEmail;
+use MsgPhp\User\Infrastructure\Validator\UniqueUsername;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,7 +17,7 @@ final class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('email', EmailType::class, [
-            'constraints' => [new NotBlank(), new Email(), new UniqueEmail()],
+            'constraints' => [new NotBlank(), new Email(), new UniqueUsername()],
         ]);
         $builder->add('password', HashedPasswordType::class, [
             'password_confirm' => true,
