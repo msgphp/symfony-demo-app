70aa1179da3313cb9effde5841c3d57816e379d7
Roland Franssen <franssen.roland@gmail.com>
2020-03-08 14:29:01 +0100

    update to 0.15

--- src/Kernel.php
+++ src/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends BaseKernel
 
     public function registerBundles(): iterable
     {
-        /** @psalm-suppress UnresolvableInclude */
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
